541e116039526980bcd3dbf6f3f954a8538b2e61
Lucio Morales <lumordevelop@gmail.com>
2026-07-06 22:43:58 -0300

    reset changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-// import Dashboard from "./Dashboard";
-// import Features from "./Features";
-// import Roles from "./Roles";
-// import StoreSection from "./StoreSection";
-// import Hero from "./Hero";
-// import Navbar from "./Navbar";
-
 import { LoginPage } from "./features/auth";
 
 function App() {
   return (
     <LoginPage />
-
-
   );
 }
-
-// <div className="min-h-screen bg-slate-950 text-slate-100 overflow-x-hidden">
-
-{/* <Navbar />
-
-      <Hero /> */}
-
-{/* <Features />
-
-      <Dashboard />
-
-      <Roles />
-
-      <StoreSection /> */}
-
-// </div>
 
 export default App;
